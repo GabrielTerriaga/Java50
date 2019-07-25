f8a91e7dce0e1236e9a2aa096c092f4c0e63459a
Gabriel Terriaga <Gmterriaga0@yahoo.com.br>
2019-07-25 01:35:34 -0300

    Expressao lambda

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import entities.Product;
@@ -18,8 +19,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
+		Function<Product, String> func = p -> p.getName().toUpperCase();
 		//function com map + static method
-		List<String> namesUp = list.stream().map(Product::nonStaticUpperCase).collect(Collectors.toList());
+		List<String> namesUp = list.stream().map(func).collect(Collectors.toList());
 		
 		namesUp.forEach(System.out::println);
 	}
